0603521ba8a22815a41cc9757074b6714ddc8915
wamakimaN <wamamoses@gmail.com>
2020-01-06 00:09:41 +0300

    creasted a test case for initialization of News object so it fails

---
+++ app/news_test.py
@@ -0,0 +1,16 @@
+import unittest
+from models import news
+News = news.News
+
+
+class NewsTest(unittest.TestCase):
+    """
+    test clacc to  test bevaviour of the News class
+    """
+    def setUp(self):
+        self.new_news = News("somebody",
+        "once told me that","not to get worked-up","https://www.onsmallthings.com","https://www.itwillsave.com/youthetrouble","2020-01-26T07:58:21Z")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news, News))
+    
