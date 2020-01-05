d43aa528550aad7475df15c148b0a4479263389d
wamakimaN <wamamoses@gmail.com>
2020-01-06 01:45:29 +0300

    create test to check if Source initializes well

--- app/news_test.py
+++ app/news_test.py
@@ -1,6 +1,7 @@
 import unittest
 from models import news,source
 News = news.News
+Source = source.Source
 
 class NewsTest(unittest.TestCase):
     """
@@ -13,3 +14,18 @@ class NewsTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news, News))
 
+class SourcesTest(unittest.TestCase):
+    """
+    Test Case to test the behavior of the sources class
+    """
+    def setUp(self):
+        """
+        setUp method that will run before every test
+        """
+        self.new_sources = Source("abc-news","ABC News", "abcnews.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_sources, Source))
+
+if __name__ == '__main__':
+    unittest.main()
