911a9b5f6e7172fa8d9a8fa889f11dbed8ad7dab
wamakimaN <wamamoses@gmail.com>
2020-01-06 00:10:43 +0300

    create initialization method for News object to pass the test

--- app/models/news.py
+++ app/models/news.py
@@ -1,1 +1,12 @@
+class News:
+    
+    #News class to define News objects
+    
 
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
