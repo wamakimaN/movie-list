36fe27d1afc49bcb1f4854d747e105bbbf4f306e
wamakimaN <wamamoses@gmail.com>
2020-01-05 23:15:54 +0300

    finish initializing app

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,9 @@ from flask import Flask
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+
 from app import views
